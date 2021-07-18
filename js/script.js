3895babca124f4a6173efc4e83485ed31f234904
stepan323446 <66179087+stepan323446@users.noreply.github.com>
2021-07-18 18:59:59 +0700

    .

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new Swiper('.site-slider', {
+    pagination:
+    {
+        el: ".swiper-pagination",
+        type: "progressbar",    
+    },
+    mousewheel: 
+    {
+        // Чувствительность
+        sensitivity: 0.5,
+    },
+    speed: 800,
+});
